8586640df89ce14801ced5b23ea190ede0b16cea
motiasbaha <motiasb64@gmail.com>
2021-11-07 12:08:12 +0200

    added .env

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express =require('express');
 const app = express();
+require('dotenv').config();
 
-app.get('/' , (req,res)=>{
+app.get('*' , (req,res)=>{
     res.send('welcome')
 });
 
-app.listen(5000, ()=>{
+
+const PORT = process.env.PORT
+app.listen(PORT, ()=>{
     console.log('listining on port 5000')
 })
